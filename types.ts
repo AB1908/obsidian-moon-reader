00ad8e62c13a311008030e474f21b4fef27256ee
AB1908 <14124383+AB1908@users.noreply.github.com>
2022-03-16 19:17:11 +0530

    Add annotation class def

---
+++ types.ts
@@ -0,0 +1,19 @@
+export class Annotation {
+    //todo: check types
+    indexCount: number;
+    bookName: string;
+    bookPath: string;
+    lowerCasePath: string;
+    sectionNumber: number;
+    placeholder1: string;
+    location: string;
+    characterCount: number;
+    signedColor: number;
+    unixTimestamp: string;
+    bookmarkText: string;
+    noteText: string;
+    highlightText: string;
+    annotType1: number;
+    annotType2: number;
+    annotType3: number;
+}
