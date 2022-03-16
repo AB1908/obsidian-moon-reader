23ca702ad3c205195987fd1132405d8f9b773d3e
AB1908 <14124383+AB1908@users.noreply.github.com>
2022-03-16 19:17:17 +0530

    Add helper function

---
+++ util.ts
@@ -0,0 +1,11 @@
+export default function integerToRGBA(
+    number: number
+): string {
+  if (number < 0)
+  {
+    number = 0xFFFFFFFF + number + 1;
+  }
+  const ARGB = number.toString(16).toUpperCase();
+
+  return `#${ARGB.slice(2,)}${ARGB.slice(0,2)}`;
+}
