6707c08fcf01ff2f01bb287eeba8f318e4c08c92
AB1908 <14124383+AB1908@users.noreply.github.com>
2022-11-25 22:16:37 +0530

    test: rewrite parser test to use example file

--- src/test/parser.test.ts
+++ src/test/parser.test.ts
@@ -1,6 +1,6 @@
 import {expect, test} from "@jest/globals";
-import parserOutput from "./json/howtotakenotes-success/parser.parse.output-2022-11-19 14-04-29.json";
-import {highlightContent} from "./json/howtotakenotes-success/mrexpt.json";
+import parserOutput from "./json/parseMrexptContents.parseMrexptContents.output-2022-11-25 22-07-41.json";
+import {highlightContent} from "./json/parseMrexptContents.parseMrexptContents.input-2022-11-25 22-07-41.json";
 import {parseMrexptContents} from "../parseMrexptContents";
 
 test("regular MoonReader import parsing", () => {
